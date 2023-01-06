8f01db04f80bc6679b903bcab45556448e23f358
Jon Andoni Galdos <jonandonigv@gmail.com>
2023-01-06 21:24:27 +0100

    stuff

--- Nextjs/CreateNextApp/nextjs-blog/pages/api/hello.js
+++ Nextjs/CreateNextApp/nextjs-blog/pages/api/hello.js
@@ -1,4 +1,4 @@
-// req = HTTP incoming message, res = HTTP server response
+// req = HTTP incoming message, res = HTTP server response s
 export default function handler(req, res) {
     res.status(200).json({text: 'hello'});
 }
