042d6edab912368839a22e9ccb20a66fffc11d26
Jon Andoni Galdos <jonandonigv@gmail.com>
2023-01-08 20:22:25 +0100

    The Basics

---
+++ TypeScript/TheTypeScriptHandBook/hello.js
@@ -0,0 +1,7 @@
+// Greets the world
+console.log('Hello world!');
+// This is an industrial-grade general-purpose greeter function
+function greet(person, date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+greet("Brendan", new Date);
---
+++ TypeScript/TheTypeScriptHandBook/hello.ts
@@ -0,0 +1,9 @@
+// Greets the world
+console.log('Hello world!');
+
+// This is an industrial-grade general-purpose greeter function
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet("Brendan", new Date);
